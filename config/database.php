02dba472665147a29f9c6406fffd1c2dd9a5e4cb
Jan <strymkr.john@gmail.com>
2020-06-22 23:51:49 +0800

    initial commit

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => 'localhost',
             'port'      => '3306',
-            'database'  => 'mut_db',
+            'database'  => 'merchanthelp_db',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8mb4',
